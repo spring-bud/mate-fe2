8f6c192097c4615f58eaa993e9191bd09171fc51
clyde-yoonjae <leewh77777@gmail.com>
2025-03-28 18:13:39 +0900

    chore logo-section in header

--- components/common/header/logo.tsx
+++ components/common/header/logo.tsx
@@ -1,19 +1,19 @@
 "use client";
 
+import React from "react";
 import Link from "next/link";
-import logoImage from "@/assets/images/laptop-cat.png";
 import Image from "next/image";
+import LogoImage from "@/assets/images/laptop-cat.png";
 
-const LogoComponent = () => {
+const LogoComponent: React.FC = () => {
   return (
     <Link href="/" className="flex items-center">
       <Image
-        src={logoImage}
+        src={LogoImage}
         alt="Logo"
         width={40}
         height={40}
         className="h-10 w-auto"
-        priority
       />
       <span className="ml-2 typo-head3 text-textPrimary dark:text-textLight">
         MATE
