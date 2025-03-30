09833ca446c4587c550cee87ddd9017b8a41d0cf
clyde-yoonjae <leewh77777@gmail.com>
2025-03-30 16:11:27 +0900

    feat https setting

---
+++ server.mjs
@@ -0,0 +1,24 @@
+import https from 'https';
+import { parse } from 'url';
+import next from 'next';
+import fs from 'fs';
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+const httpsOptions = {
+  key: fs.readFileSync('./localhost-key.pem'),
+  cert: fs.readFileSync('./localhost.pem'),
+};
+
+app.prepare().then(() => {
+  https
+    .createServer(httpsOptions, (req, res) => {
+      const parsedUrl = parse(req.url, true);
+      handle(req, res, parsedUrl);
+    })
+    .listen(3000, () => {
+      console.log('🚀 HTTPS 서버가 실행되었습니다: https://localhost:3000');
+    });
+});
