8c41bdf73d36a6f84bf6ef479cec5eb95d8d2774
clyde-yoonjae <leewh77777@gmail.com>
2025-04-01 19:24:46 +0900

    chore tiptap index

---
+++ components/common/tiptap/index.ts
@@ -0,0 +1,8 @@
+export { default as TiptapEditor } from './TiptapEditor';
+export { default as Toolbar } from './Toolbar';
+export { getExtensions } from './tiptap-function/extensions';
+export {
+  uploadImageToEditor,
+  mockUploadImage,
+} from './tiptap-function/imageUploader';
+export * from '../../../hooks/editor/useEditorHooks';
