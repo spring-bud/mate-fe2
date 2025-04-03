baf1854e1beeac223d8810b3f947a7f5fb09c0ad
clyde-yoonjae <leewh77777@gmail.com>
2025-04-03 21:46:36 +0900

    feat set user in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,37 +1,39 @@
-import type { Metadata } from "next";
-import { Viewport } from "next";
-import "./globals.css";
-import Header from "@/components/common/header/Header";
+import type { Metadata } from 'next';
+import { Viewport } from 'next';
+import './globals.css';
+import Header from '@/components/common/header/Header';
+import { cookies } from 'next/headers';
+import { getUserFromToken } from '@/utils/server-jwt';
 
 export const metadata: Metadata = {
-  title: "MATE | 개발자와 디자이너를 위한 프리랜서 허브",
+  title: 'MATE | 개발자와 디자이너를 위한 프리랜서 허브',
   description:
-    "MATE는 개발자와 디자이너를 프로젝트와 외주 기회에 연결해주는 전문 플랫폼입니다. 최고의 인재들과 함께 여러분의 비전을 실현하세요.",
+    'MATE는 개발자와 디자이너를 프로젝트와 외주 기회에 연결해주는 전문 플랫폼입니다. 최고의 인재들과 함께 여러분의 비전을 실현하세요.',
   keywords: [
-    "프리랜서",
-    "개발자",
-    "디자이너",
-    "웹개발",
-    "앱개발",
-    "UI/UX",
-    "외주",
-    "프로젝트",
-    "프리랜서 플랫폼",
+    '프리랜서',
+    '개발자',
+    '디자이너',
+    '웹개발',
+    '앱개발',
+    'UI/UX',
+    '외주',
+    '프로젝트',
+    '프리랜서 플랫폼',
   ],
   openGraph: {
-    title: "MATE | 개발자와 디자이너를 위한 프리랜서 허브",
+    title: 'MATE | 개발자와 디자이너를 위한 프리랜서 허브',
     description:
-      "최고의 개발자와 디자이너를 만나 프로젝트를 성공적으로 완성하세요.",
+      '최고의 개발자와 디자이너를 만나 프로젝트를 성공적으로 완성하세요.',
     // url: 'https://mate-freelancer.com',
-    siteName: "MATE",
-    locale: "ko_KR",
-    type: "website",
+    siteName: 'MATE',
+    locale: 'ko_KR',
+    type: 'website',
   },
   twitter: {
-    card: "summary_large_image",
-    title: "MATE | 개발자와 디자이너를 위한 프리랜서 허브",
+    card: 'summary_large_image',
+    title: 'MATE | 개발자와 디자이너를 위한 프리랜서 허브',
     description:
-      "최고의 개발자와 디자이너를 만나 프로젝트를 성공적으로 완성하세요.",
+      '최고의 개발자와 디자이너를 만나 프로젝트를 성공적으로 완성하세요.',
   },
   robots: {
     index: true,
@@ -41,20 +43,31 @@ export const metadata: Metadata = {
 
 // viewport와 themeColor를 별도로 내보내기
 export const viewport: Viewport = {
-  width: "device-width",
+  width: 'device-width',
   initialScale: 1,
-  themeColor: "#1E1E1E",
+  themeColor: '#1E1E1E',
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const cookieStore = await cookies();
+  const accessToken = cookieStore.get('access_token');
+  let userInfo = null;
+  if (accessToken?.value) {
+    try {
+      userInfo = getUserFromToken(accessToken.value);
+    } catch (error) {
+      console.error('토큰 디코딩 오류:', error);
+    }
+  }
+
   return (
-    <html lang="ko" className="dark">
+    <html lang='ko' className='dark'>
       <body>
-        <Header />
+        <Header initialUserInfo={userInfo} />
         {children}
       </body>
     </html>
