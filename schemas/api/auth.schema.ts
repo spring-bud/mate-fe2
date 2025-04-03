9d091893c2dc41fcabed4d99d1b62d4946f83be5
clyde-yoonjae <leewh77777@gmail.com>
2025-04-03 21:47:10 +0900

    chore access Token schema

--- schemas/api/auth.schema.ts
+++ schemas/api/auth.schema.ts
@@ -1,7 +1,17 @@
-import { z } from "zod";
+import { z } from 'zod';
 
 export const reissueResponseSchema = z.object({
   access_token: z.string(),
 });
 
+export const accessTokenPayloadSchema = z.object({
+  user_id: z.number(),
+  user_url: z.string().url(),
+  user_nickname: z.string(),
+  sub: z.string(),
+  iat: z.number(),
+  exp: z.number(),
+});
+
 export type ReissueResponse = z.infer<typeof reissueResponseSchema>;
+export type AccessTokenPayload = z.infer<typeof accessTokenPayloadSchema>;
