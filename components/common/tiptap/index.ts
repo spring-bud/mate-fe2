9a5ce77fd7107529ad18f5101750703c55ce8252
clyde-yoonjae <leewh77777@gmail.com>
2025-04-11 21:09:08 +0900

    chore uploadImg for edditor

--- components/common/tiptap/index.ts
+++ components/common/tiptap/index.ts
@@ -1,8 +1,5 @@
 export { default as TiptapEditor } from './TiptapEditor';
 export { default as Toolbar } from './Toolbar';
 export { getExtensions } from './tiptap-function/extensions';
-export {
-  uploadImageToEditor,
-  mockUploadImage,
-} from './tiptap-function/imageUploader';
+
 export * from '../../../hooks/editor/useEditorHooks';
