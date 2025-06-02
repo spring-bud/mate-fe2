78410e7e885225be7d18c02704051acd9118d28e
clyde-yoonjae <leewh77777@gmail.com>
2025-06-02 15:53:08 +0900

    chore createProduct mutation for invalidate

--- hooks/mutation/useCreateProduct.ts
+++ hooks/mutation/useCreateProduct.ts
@@ -5,14 +5,20 @@ import { productURL } from '@/service/endpoints/endpoints';
 import { apiClient } from '@/utils/api/api';
 import { PostFormData } from '@/schemas/validations/postForm.schema';
 import { productResponseSchema } from '@/schemas/api/product.schema';
+import { queryKeys } from '@/lib/react-query/queryKeys';
+import { useQueryClient } from '@tanstack/react-query';
 
 const useCreateProduct = () => {
+  const queryClient = useQueryClient();
   return useMutation({
     mutationFn: async (data: PostFormData) => {
       return apiClient.post(productURL.create, {
         params: data,
         schema: productResponseSchema,
       });
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: queryKeys.products.all });
     },
   });
 };
